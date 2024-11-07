c6b216e2ed9517c95733c2446d8dc404787a20be
gbrl-mar <220711815@students.uajy.ac.id>
2024-11-07 15:38:02 +0700

    Update Tugas_Modul4_A_Mar_11815.py

--- Tugas_Modul4_A_Mar_11815.py
+++ Tugas_Modul4_A_Mar_11815.py
@@ -71,12 +71,11 @@ if uploaded_file is not None:
     )
     st.dataframe(input_data)
 
-    # Direktori tempat penyimpanan ketiga model yang telah di dump sebelumnya
-    model_directory = r"C:\PBP\Unsupervised Learning (Praktek)\Tugas4_X_YYYYY"
+  
     model_path = {
-        "AGG_model": os.path.join(model_directory, r"AGG_model.pkl"),
-        "KMeans_model": os.path.join(model_directory, r"KMeans_model.pkl"),
-        "DBSCAN_model": os.path.join(model_directory, r"DBSCAN_model.pkl"),
+        "AGG_model":  r"AGG_model.pkl",
+        "KMeans_model":  r"KMeans_model.pkl",
+        "DBSCAN_model":  r"DBSCAN_model.pkl",
     }
 
     models = {}
